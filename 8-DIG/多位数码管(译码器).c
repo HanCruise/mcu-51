bf1d19ce5305b3d66375822ed318a6f97d800c0b
HanCruise <wanglx1018@163.com>
2017-04-13 19:39:25 +0800

    Create 多位数码管(译码器).c

---
+++ 8-DIG/多位数码管(译码器).c
@@ -0,0 +1,40 @@
+#include <reg51.h>
+
+#define uint unsigned int
+#define uchar unsigned char
+
+#define GPIO_DIG   P0
+sbit LSA=P1^0;
+sbit LSB=P1^1;
+sbit LSC=P1^2;
+
+uchar code DIG_CODE[16]=
+{0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,
+0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71};
+
+void main(void)
+{
+	uint i=1,j;
+	while(1)
+	{
+		switch(i)
+		{
+			case 1:LSA=0;LSB=0;LSC=0;break;
+			case 2:LSA=1;LSB=0;LSC=0;break;
+			case 3:LSA=0;LSB=1;LSC=0;break;
+			case 4:LSA=1;LSB=1;LSC=0;break;
+			case 5:LSA=0;LSB=0;LSC=1;break;
+			case 6:LSA=1;LSB=0;LSC=1;break;
+			case 7:LSA=0;LSB=1;LSC=1;break;
+			case 8:LSA=1;LSB=1;LSC=1;break;
+			default:break;
+		}
+		GPIO_DIG=DIG_CODE[i];
+		j=10;
+		while(j--);
+		GPIO_DIG=0x00;
+		i++;
+		if(i==9) i=1;
+	}
+}
+
